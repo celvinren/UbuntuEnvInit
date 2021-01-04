e54705ca3c49aeb8be5e146e20c95ae5557b62a5
celvinren <celvinren@hotmail.com>
2021-01-04 22:48:04 +1100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,29 @@
+sudo passwd root
+su root
+
+#####allow root login
+#nano /etc/ssh/sshd_config
+#####add in the following lines
+#LoginGraceTime 120
+#PermitRootLogin yes
+#StrictModes yes
+
+
+echo install ssh server
+sudo apt install openssh-server
+
 echo Install google chrome
+cd /home/calvin/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb -y
 
 echo install VS Code
+cd /home/calvin/Downloads
 wget https://az764295.vo.msecnd.net/stable/ea3859d4ba2f3e577a159bc91e3074c5d85c0523/code_1.52.1-1608136922_amd64.deb
 sudo dpkg -i code_1.52.1-1608136922_amd64.deb -y
 
 echo install golang
+cd /home/calvin/Downloads
 wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
@@ -16,14 +33,16 @@ sudo apt install git -y
 sudo apt install gcc -y
 
 echo install flutter
+cd /home/calvin/Documents
 wget https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_1.22.5-stable.tar.xz
 tar xf flutter_linux_1.22.5-stable.tar.xz
-export PATH="$PATH:[PATH_TO_FLUTTER_GIT_DIRECTORY]/flutter/bin"
+export PATH="$PATH:/home/calvin/Documents/flutter/bin"
 
 echo install snap
 sudo apt install snapd
 
 echo install virtualbox and genymotion emulator
+cd /home/calvin/Downloads
 sudo apt install virtualbox
 wget https://dl.genymotion.com/releases/genymotion-3.1.2/genymotion-3.1.2-linux_x64.bin
 chmod +x /home/calvin/Downloads/genymotion-3.1.2-linux_x64.bin
