428727fca9d6faead8e4f720222de5df613aaa8f
celvinren <celvinren@hotmail.com>
2021-01-05 11:58:22 +1100

    Update init.sh

--- init.sh
+++ init.sh
@@ -73,6 +73,12 @@ sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
+echo install docker machine
+base=https://github.com/docker/machine/releases/download/v0.16.0
+curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
+sudo mv /tmp/docker-machine /usr/local/bin/docker-machine
+chmod +x /usr/local/bin/docker-machine
+
 #go get -u github.com/swaggo/swag/cmd/swag
 #swag init
 
